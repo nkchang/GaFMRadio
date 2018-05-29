d8a0471e27c2edced012a0bcac92281f0796494a
Nick Chang <nkchang@gmail.com>
2018-05-29 13:33:24 +0800

    Update deeplink website address with right assetlink file.

--- app/src/main/java/nkchang/idv/gafmradio/MainActivity.java
+++ app/src/main/java/nkchang/idv/gafmradio/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Launch deep link view manually ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
